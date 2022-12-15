68dba3c3eec579298303537b74695f1ff5499ef8
Giulia <Giulia@LAPTOP-QH98NRHJ>
2022-12-15 17:38:53 +0100

    Creazione categoryCtr

---
+++ src/main/java/com/example/demo/green/CategoryCtr.java
@@ -0,0 +1,26 @@
+package com.example.demo.green;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.green.repo.CategoryRepo;
+
+@Controller
+@RequestMapping("/green/home")
+public class CategoryCtr {
+	
+	private CategoryRepo repo;
+	
+	public CategoryCtr(CategoryRepo repo) {
+		this.repo = repo;
+	}
+	
+	@GetMapping()
+	public String getAll(Model model) {
+		model.addAttribute("categories", repo.findAll());
+		return "/green/menu";
+	}
+	
+}
