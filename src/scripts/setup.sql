03b922e5e1eb97b10ba0eb53ad3b57b329648b2f
AlToscani <93136804+AlToscani@users.noreply.github.com>
2022-12-15 15:43:23 +0100

    created setup script

---
+++ src/scripts/setup.sql
@@ -0,0 +1,40 @@
+drop table if exists detail;
+DROP TABLE IF EXISTS MENU;
+drop table if exists category;
+drop table if exists "order";
+
+create table category(
+	category_id serial primary key,
+	"name" varchar(20)
+);
+
+insert into category (name) values ('Antipasto e fritto');
+insert into category (name) values ('Pizza');
+insert into category (name) values ('Pinsa');
+insert into category (name) values ('Dolce');
+insert into category (name) values ('Bevanda');
+
+create table menu(
+	menu_id serial primary key,
+	"name" varchar(25),
+	price decimal,
+	category_id integer,
+	foreign key (category_id) references category (category_id) on delete cascade
+);
+
+insert into menu (name, price, category_id) values ('pizza margherita', 6.50, 2);
+
+create table "order"(
+	order_id serial primary key,
+	total decimal
+);
+
+create table detail(
+	detail_id serial primary key,
+	order_id integer,
+	menu_id integer,
+	quantity decimal,
+	foreign key (order_id) references "order" (order_id) delete on cascade,
+	foreign key (menu_id) references menu (menu_id) delete on cascade
+);
+
