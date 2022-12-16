2cd6382b5a4422098579d229095f98aba43c10c3
Giulia <Giulia@LAPTOP-QH98NRHJ>
2022-12-16 10:56:43 +0100

    dao modificato

--- src/main/java/com/example/demo/green/dao/Category.java
+++ src/main/java/com/example/demo/green/dao/Category.java
@@ -15,13 +15,19 @@ public class Category {
 	@Column(name = "CATEGORY_ID")
 	private Integer id;
 	private String name;
+	private String imgpath;
+	private String description;
+	private String href;
 	
 	public Category() {
 		
 	}
 	
-	public Category(String name) {
+	public Category(String name, String imgpath, String description, String href) {
 		this.name = name;
+		this.imgpath = imgpath;
+		this.description = description;
+		this.href = href;
 	}
 
 	public Integer getId() {
@@ -40,9 +46,33 @@ public class Category {
 		this.name = name;
 	}
 
+	public String getImgpath() {
+		return imgpath;
+	}
+
+	public void setImgpath(String imgpath) {
+		this.imgpath = imgpath;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, name);
+		return Objects.hash(description, href, id, imgpath, name);
 	}
 
 	@Override
@@ -54,12 +84,17 @@ public class Category {
 		if (getClass() != obj.getClass())
 			return false;
 		Category other = (Category) obj;
-		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+		return Objects.equals(description, other.description) && Objects.equals(href, other.href)
+				&& Objects.equals(id, other.id) && Objects.equals(imgpath, other.imgpath)
+				&& Objects.equals(name, other.name);
 	}
 
 	@Override
 	public String toString() {
-		return "Category [id=" + id + ", name=" + name + "]";
+		return "Category [id=" + id + ", name=" + name + ", imgpath=" + imgpath + ", description=" + description
+				+ ", href=" + href + "]";
 	}
+
+	
 	
 }
