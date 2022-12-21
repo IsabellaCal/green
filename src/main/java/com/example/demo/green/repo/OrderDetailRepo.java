a3dd64d6664750859df4c60d378d88e46a83155f
AlToscani <93136804+AlToscani@users.noreply.github.com>
2022-12-21 12:44:14 +0100

    commento query on funzionante, DA FIXARE

--- src/main/java/com/example/demo/green/repo/OrderDetailRepo.java
+++ src/main/java/com/example/demo/green/repo/OrderDetailRepo.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.demo.green.dao.OrderDetail;
 
 public interface OrderDetailRepo extends CrudRepository<OrderDetail, Integer> {
-	@Query("select od from OrderDetail od where od.ordineId = ?1")
-	Iterable<OrderDetail> findDetailsById(Integer id);
+//	@Query("select od from OrderDetail od where od.ordineId = ?1")
+//	Iterable<OrderDetail> findDetailsById(Integer id);
 }
